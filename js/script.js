77112e33574ea9a4cad08402372625f0c9340be4
Shahzad <karimbayewshahzad@gmail.com>
2022-12-18 01:43:32 +0500

    add films function in input

--- js/script.js
+++ js/script.js
@@ -14,44 +14,95 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll('.promo__adv img'),
+          genre = document.querySelector('.promo__genre'),
+          poster = document.querySelector('.promo__bg'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"');
 
-const adv = document.querySelectorAll('.promo__adv img'),
-      genre = document.querySelector('.promo__genre'),
-      poster = document.querySelector('.promo__bg'),
-      movieList = document.querySelector('.promo__interactive-list');
+    addForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+        
+        if (newFilm) {
 
-//  reklamalarny udalit atmak ucin
-adv.forEach(item => {
-    item.remove();  
-});
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
 
-// text almashdyrmak ucin
-genre.textContent = 'Драма'; 
+            if (favorite) {
+                console.log('Добавляем любимым фильм');
+            }
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+    
+            creteMovieList(movieDB.movies, movieList);
+        }
 
-// backgrounda suratyny almashdirmak ucin
-poster.style.backgroundImage = 'url("img/bg.jpg")'; 
-
-// icindagilani boshatmak ucin
-movieList.innerHTML = ''; 
-
-// sortirowka atmak ucin
-movieDB.movies.sort(); 
+        e.target.reset();
+    });
 
 
-//kino atlaryny dobawit atmak ucin
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `; 
+    const deleteAdv = (arr) => {
+        //  reklamalarny udalit atmak ucin
+        arr.forEach(item => {
+        item.remove();  
+        });
+    };
+
+    deleteAdv(adv);
+    
+    const makeChanges = () => {
+        // text almashdyrmak ucin
+        genre.textContent = 'Драма'; 
+            
+        // backgrounda suratyny almashdirmak ucin
+        poster.style.backgroundImage = 'url("img/bg.jpg")'; 
+    };
+
+    makeChanges();
+
+    const sortArr = (arr) => {
+        // sortirowka atmak ucin
+        arr.sort(); 
+    };
+
+    function creteMovieList(films, parent) {
+        parent.innerHTML = ''; 
+        sortArr(films);
+        //kino atlaryny dobawit atmak ucin
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `; 
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                creteMovieList(films, parent);
+            });
+        });
+    }
+
+    creteMovieList(movieDB.movies, movieList);
 });
